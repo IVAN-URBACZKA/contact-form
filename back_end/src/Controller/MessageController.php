eabe64d4837ed67427245043d308980490728c83
unknown <ivan.urbaczka@gmail.com>
2022-07-11 09:26:31 +0200

    fix bug

--- back_end/src/Controller/MessageController.php
+++ back_end/src/Controller/MessageController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Message;
-use App\Form\MessageType;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -36,4 +36,34 @@ class MessageController extends AbstractController
 
         return new Response('Done.', Response::HTTP_OK);
     }
+
+
+   /**
+     *  JSON sender - handler
+     * @Route("/api/message/search/{searchTerm}", name="api_message_search")
+     */
+    public function searchmessage(EntityManagerInterface $entityManager, $searchTerm): Response
+    {
+        $query = $entityManager->createQuery("SELECT m FROM App:Message AS m WHERE LOWER(m.email) LIKE :search");
+        $query->setParameter('search',  $searchTerm );
+        $messages = $query->getResult();
+
+        $data = [];
+
+        foreach ($messages as $message) {
+            $data[] = [
+                'id' => $message->getId(),
+                'name' => $message->getName(),
+                'email' => $message->getEmail(),
+                'subject' => $message->getSubject(),
+                'description' => $message->getDescription(),
+
+            ];
+        }
+        $dataenc = json_encode($data);
+        return new Response($dataenc, Response::HTTP_OK);
+    }
+
+
+
 }
